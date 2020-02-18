1e28b64c8e16b4f6c9f3833c1b43322c4619829b
Marcelo Paniagua <marcelo.zometa@hotmail.com>
2020-02-17 20:13:54 -0700

    Created home page

---
+++ El_Salvador_TourGuide/El_Salvador_TourGuide/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace El_Salvador_TourGuide.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
